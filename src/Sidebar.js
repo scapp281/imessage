04a447b82bdb71fede5b66ab4169e4838fde2742
Stephen Chin <schin281@gmail.com>
2020-10-13 21:58:05 -0400

    Add logout functionality

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,13 +4,19 @@ import SidebarChat from './SidebarChat';
 import { Avatar, IconButton } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import RateReviewTwoToneIcon from '@material-ui/icons/RateReviewTwoTone';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import { auth } from './firebase';
 
 function Sidebar() {
+
+  const user = useSelector(selectUser);
+
   return (
     <div className="sidebar">
       
       <div className="sidebar__header">
-        <Avatar className="sidebar__avatar" />
+        <Avatar onClick={ () => auth.signout() } src={user.photo} className="sidebar__avatar" />
         <div className="sidebar__input">
             <SearchIcon />
             <input placeholder="search" />
